94e79fea3bee2bc180200567452f38321d7aa1ce
Marcin <marcinegzmont@gmail.com>
2022-12-17 18:34:11 +0000

    db, empty tables, models

---
+++ db/dvd_shop.sql
@@ -0,0 +1,24 @@
+DROP TABLE films;
+DROP TABLE directors;
+DROP TABLE distributors;
+
+
+CREATE TABLE directors (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE distributors (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE films (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  director_id INT REFERENCES directors(id) ON DELETE CASCADE,
+  distributor_id INT REFERENCES distributors(id) ON DELETE CASCADE,
+  stock_quantity INT,
+  buying_price INT,
+  selling_price INT
+);
---
+++ models/film.py
@@ -0,0 +1,9 @@
+class Film:
+    def __init__(self, title, director, distributor, stock_quantity, buying_price, selling_price, id = None ):
+        self.title = title
+        self.director = director
+        self.distributor = distributor
+        self.stock_quantity = stock_quantity
+        self.buying_price = buying_price
+        self.selling_price = selling_price
+        self.id = id
