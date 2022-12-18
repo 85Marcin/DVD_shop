27c783555b24b803b32916db7bcf98213ceab848
Marcin <marcinegzmont@gmail.com>
2022-12-18 17:59:44 +0000

    added stock table

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 
+import repositories.film_repository as film_repository
 
 
 films_blueprint = Blueprint("films", __name__)
+
+@films_blueprint.route("/films")
+def films():
+    films = film_repository.select_all()
+    return render_template("stock/index.html", films = films)
 
