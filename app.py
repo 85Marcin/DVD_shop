3714b053f2f24e7bcd7de28b835f37d46f94aab0
Marcin <marcinegzmont@gmail.com>
2022-12-18 14:19:54 +0000

    added controller.py

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template
 
+from controllers.controller import films_blueprint
+
 app = Flask(__name__)
+
+
+
+app.register_blueprint(films_blueprint)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
